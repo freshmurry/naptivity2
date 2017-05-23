ac7fad7e87fa05f8da873903abafad3f2984d588
FreshMurry <lawrencemurry@yahoo.com>
2017-05-23 15:09:22 +0000

    Updated Controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
   	def configure_permitted_parameters
-  		devise_parameter_sanitizer.for(:sign_up) << :fullname
-  		devise_parameter_sanitizer.for(:account_update) << :fullname << :phone_number << :description << :email << :password
+  		devise_parameter_sanitizer(:sign_up, keys: [:fullname])
+  		devise_parameter_sanitizer(:account_update, keys: [:fullname, :phone_number, :description, :email, :password])
   	end
 end
