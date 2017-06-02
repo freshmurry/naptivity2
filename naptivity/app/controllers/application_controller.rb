88e80d361e5e9755cb8cc25a06d54e25dcde7d17
FreshMurry <lawrencemurry@yahoo.com>
2017-06-02 02:47:57 +0000

    Adding files one level up from Naptivity in order to push refs to Heroku

---
+++ naptivity/app/controllers/application_controller.rb
@@ -0,0 +1,30 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << :fullname
+      devise_parameter_sanitizer.for(:account_update) << :fullname << :phone_number << :description << :email << :password
+    end
+end
+
+# For Devise 4.1 and higher
+
+# class ApplicationController < ActionController::Base
+#   # Prevent CSRF attacks by raising an exception.
+#   # For APIs, you may want to use :null_session instead.
+#   protect_from_forgery with: :exception
+
+#   before_action :configure_permitted_parameters, if: :devise_controller?
+
+#   protected
+#     def configure_permitted_parameters
+#       devise_parameter_sanitizer(:sign_up, keys: [:fullname])
+#       devise_parameter_sanitizer(:account_update, keys: [:fullname, :phone_number, :description, :email, :password])
+#     end
+# end
+
